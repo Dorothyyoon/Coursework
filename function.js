97df121391514f034f6c3170b364d5c178d27093
Dorothyyoon <84230917+Dorothyyoon@users.noreply.github.com>
2021-06-12 00:43:20 +0630

    Create function.js

---
+++ function.js
@@ -0,0 +1,10 @@
+function Book (){
+  var name = 'To the road';
+  var type = 'novel';
+  var thick = '2in';
+  var review = 'very good';
+  console.log('name: ',name)
+  console.log('type:' ,type);
+  console.log('thick:' ,thick);
+  console.log('review:' ,review);
+}

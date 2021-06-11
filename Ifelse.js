be240129f55574183ad516700aa98b457ed9d8c3
Dorothyyoon <84230917+Dorothyyoon@users.noreply.github.com>
2021-06-12 00:14:48 +0630

    Create Ifelse.js

---
+++ Ifelse.js
@@ -0,0 +1,11 @@
+alret ('I will tell you which person you are!');
+let colors = prompt('Plese enter a primary colors');
+let result = null;
+  if(colors = red){result = 'You may be a brave person';
+}
+  else if(colors = yellow){result = 'You may be a happy person';
+}
+  else if(colors = blue){result = 'You may be a cool person';
+}
+    else{result = 'This is not a primary color!';
+}
